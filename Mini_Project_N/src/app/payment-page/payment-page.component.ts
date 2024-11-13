8a567c078f7313273e61e1453813b5a8bd53e18d
Zeba Naourin <zeba.naourin7@gmail.com>
2024-11-13 11:38:47 +0530

    Updated

---
+++ Mini_Project_N/src/app/payment-page/payment-page.component.ts
@@ -0,0 +1,65 @@
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+interface Product {
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+@Component({
+  selector: 'app-payment-page',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './payment-page.component.html',
+  styleUrls: ['./payment-page.component.css']
+})
+export class PaymentPageComponent {
+  products = [
+    {
+      name: 'Product 1',
+      price: 29.99,
+      quantity: 1,
+      image: 'asserts/download.jpg',  // Image file in the assets folder
+    },
+    {
+      name: 'Product 2',
+      price: 149.99,
+      quantity: 2,
+      image: 'asserts/download2.jpg',  // Same image for this product
+    },
+  ];
+
+  totalAmount = this.calculateTotal();
+
+  increaseQuantity(index: number) {
+    this.products[index].quantity++;
+    this.totalAmount = this.calculateTotal();
+  }
+
+  decreaseQuantity(index: number) {
+    if (this.products[index].quantity > 1) {
+      this.products[index].quantity--;
+      this.totalAmount = this.calculateTotal();
+    }
+  }
+
+  calculateTotal() {
+    return this.products.reduce((acc, product) => acc + (product.price * product.quantity), 0);
+  }
+
+  shippingDetails = {
+    firstname: '',
+    lastname: '',
+    email: '',
+    phonenumber: '',
+    billingAddress: '',
+  };
+
+  placeOrder() {
+    console.log('Order placed');
+    // Razorpay integration logic goes here
+  }
+}
